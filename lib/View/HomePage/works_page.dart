a044c965c54caf0d20ce1ce0c2204b93a426b73c
secretused <t.yuta.v3510@gmail.com>
2022-11-04 02:05:36 +0900

    #5 create change body-screen

--- lib/View/HomePage/works_page.dart
+++ lib/View/HomePage/works_page.dart
@@ -15,57 +15,66 @@ class WorksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      scrollDirection: Axis.horizontal,
+      child: Row(
         children: <Widget>[
-          Container(
-            color: const Color(0xFF89C997),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: deviceHeight * 0.05, bottom: deviceHeight * 0.05),
-                  child: SizedBox(
-                    height: deviceHeight * 0.6,
-                    child: Image.network(
-                      "https://i.imgur.com/q8j8c1i.png",
-                    ),
-                  ),
-                ),
-                BodySizedBox(targetSize: deviceWidth, value: 0.1),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          Stack(
+            children: [
+              Container(
+                color: const Color(0xFF89C997),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     BodyText(
-                      deviceHeight: deviceHeight,
-                      text: '男性限定 生理管理アプリ',
-                      color: Colors.white,
-                      fontSize: deviceHeight * 0.03,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: '',
-                    ),
-                    BodyText(
-                      deviceHeight: deviceHeight,
-                      text: 'TOMONY',
+                      text: '1',
                       color: Colors.white,
                       fontSize: deviceHeight * 0.1,
+                      fontFamily: '',
                       fontWeight: FontWeight.bold,
-                      fontFamily: '',
                     ),
-                    BodyText(
-                      deviceHeight: deviceHeight,
-                      text: '2022 UIUX Design App',
-                      color: Colors.white,
-                      fontSize: deviceHeight * 0.03,
-                      fontWeight: FontWeight.normal,
-                      fontFamily: '',
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: deviceHeight * 0.05,
+                          bottom: deviceHeight * 0.05),
+                      child: SizedBox(
+                        height: deviceHeight * 0.6,
+                        child: Image.network(
+                          "https://i.imgur.com/q8j8c1i.png",
+                        ),
+                      ),
                     ),
-                    BodySizedBox(targetSize: deviceHeight, value: 0.01),
+                    BodySizedBox(targetSize: deviceWidth, value: 0.1),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        BodyText(
+                          text: '男性限定 生理管理アプリ',
+                          color: Colors.white,
+                          fontSize: deviceHeight * 0.03,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: '',
+                        ),
+                        BodyText(
+                          text: 'TOMONY',
+                          color: Colors.white,
+                          fontSize: deviceHeight * 0.1,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: '',
+                        ),
+                        BodyText(
+                          text: '2022 UIUX Design App',
+                          color: Colors.white,
+                          fontSize: deviceHeight * 0.03,
+                          fontWeight: FontWeight.normal,
+                          fontFamily: '',
+                        ),
+                        BodySizedBox(targetSize: deviceHeight, value: 0.01),
+                      ],
+                    )
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+            ],
           )
         ],
       ),
