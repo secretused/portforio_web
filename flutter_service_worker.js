5afd4a94d73d73d3b2d65f66d9bff4a12fbaadc6
secretused <secretused@users.noreply.github.com>
2022-12-04 14:19:54 +0000

    deploy: e2c91f3a705c10662d0e97adba441d3aa255b131

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,15 +19,15 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"index.html": "9609f2158fc9477224d4da81b74797fa",
-"/": "9609f2158fc9477224d4da81b74797fa",
+"index.html": "e0db6abebfdedf21704021536b30a2d5",
+"/": "e0db6abebfdedf21704021536b30a2d5",
 "version.json": "cc132570d6d238130e38adc59e1f998d",
 "manifest.json": "314a2993bf7a8a606a639502e9b9b2c9",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"main.dart.js": "71da924ddcef5d550242ab0ef3a3575a"
+"main.dart.js": "b175fbcd5a8ec8c3ca79eb87ce06f3dc"
 };
 
 // The application shell files that are downloaded before a service worker can
