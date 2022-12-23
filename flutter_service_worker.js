5566bad7252e9a1e32691de91b984a2afff379e7
secretused <secretused@users.noreply.github.com>
2022-12-23 08:37:22 +0000

    deploy: bc01151d171da9f701b3966b026295badf2eab4c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "3d43c0573926a4446c9236ba3e13535c",
+  "main.dart.js": "8c58014a47d28ffbaf0d6a2ddb2ea9bc",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
@@ -24,8 +24,8 @@ const RESOURCES = {
 "assets/assets/figma_black.svg": "7f206a7b34f3ab9fd0334fbdb99ae346",
 "assets/assets/figma.svg": "eadd22a98ff258da074fe475a56c1cf5",
 "assets/NOTICES": "8e021c5dcc32d38b4e381c64251af7a6",
-"index.html": "1895420991bc9b0e466e51d6b5c990c1",
-"/": "1895420991bc9b0e466e51d6b5c990c1",
+"index.html": "0bca7d40d1ed05ef6440c84b7d70c852",
+"/": "0bca7d40d1ed05ef6440c84b7d70c852",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "cc132570d6d238130e38adc59e1f998d"
 };
