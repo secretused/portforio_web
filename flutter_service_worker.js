c256c363a49cdfad3e789149404fa841b6eab082
secretused <secretused@users.noreply.github.com>
2023-01-03 15:42:42 +0000

    deploy: bac5e1e1c004b9449125a3dffdd1e5e24cfcba75

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,8 +20,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/NOTICES": "8e021c5dcc32d38b4e381c64251af7a6",
-"index.html": "7f8f24704ad41dcf0dc6a22651e06111",
-"/": "7f8f24704ad41dcf0dc6a22651e06111",
+"index.html": "6acb46f5fe35b2c7a7b61fec24be509d",
+"/": "6acb46f5fe35b2c7a7b61fec24be509d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "cc132570d6d238130e38adc59e1f998d"
 };
