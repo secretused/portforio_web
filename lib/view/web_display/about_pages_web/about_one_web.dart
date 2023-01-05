788f6d299e30273067442bbcbe57f96c7eb3748b
secretused <t.yuta.v3510@gmail.com>
2023-01-05 22:49:59 +0900

    git hosting build command changed

--- lib/view/web_display/about_pages_web/about_one_web.dart
+++ lib/view/web_display/about_pages_web/about_one_web.dart
@@ -9,9 +9,8 @@ class AboutOneWeb extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // deviceWidth
     var deviceWidth = MediaQuery.of(context).size.width;
-    // 821
+
     var deviceHeight = MediaQuery.of(context).size.height;
 
     return Container(
