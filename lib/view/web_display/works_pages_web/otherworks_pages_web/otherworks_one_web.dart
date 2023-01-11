2131f367956fdfe44860ea8cf6fc2e16b5118d4c
secretused <t.yuta.v3510@gmail.com>
2023-01-11 10:00:47 +0900

    git hosting build command changed

--- lib/view/web_display/works_pages_web/otherworks_pages_web/otherworks_one_web.dart
+++ lib/view/web_display/works_pages_web/otherworks_pages_web/otherworks_one_web.dart
@@ -22,7 +22,7 @@ class OtherWorksOne extends StatelessWidget {
         children: <Widget>[
           const AssetsWidget(
             heightValue: 0.8,
-            imagePath: "about/otherworks_display.png",
+            imagePath: "assets/about/otherworks_display.png",
             // imagePath: "https://i.imgur.com/POd7NXF.png",
           ),
           WidthSizedBox(targetSize: deviceWidth, value: 0.03),
